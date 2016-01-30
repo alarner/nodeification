387f7ec8b6b51ededce321405dbab4ef5b2f84e6
Aaron Larner <anlarner@gmail.com>
2016-01-30 12:26:01 -0600

    Finish up addSubscriber implementation

--- test/addSubscriber.js
+++ test/addSubscriber.js
@@ -103,4 +103,74 @@ describe('addSubscriber', function() {
 			expect(function() { addSubscriber('email', 'test@test.com', 15, ['a', 'b', 'c']); }).not.to.throw();
 		});
 	});
+	describe('other', function() {
+		it('should store subscriber in the database if only a type and a key are specified', function(done) {
+			let bookshelf = require('bookshelf')(global.knex);
+			bookshelf.plugin('registry');
+			let addSubscriber = require('../lib/addSubscriber')({
+				errors: errors,
+				knex: global.knex,
+				bookshelf: bookshelf
+			});
+			let Subscribers = require('../lib/collections/Subscribers')(bookshelf);
+
+			Promise.all([
+				addSubscriber('email', 'test@test.com'),
+				addSubscriber('text', '5551231234'),
+				addSubscriber('android-push', '4n9pc5q38ny5cw49'),
+				addSubscriber('ios-push', '4n9pc5q38ny5cw49')
+			])
+			.then(result => {
+				let s = new Subscribers();
+				s.count().then(count => {
+					expect(count).to.equal(4);
+					done();
+				})
+				.catch(done);
+			});
+		});
+
+		it('should store subscriber with user id in the database if a user id is specified', function(done) {
+			let bookshelf = require('bookshelf')(global.knex);
+			bookshelf.plugin('registry');
+			let addSubscriber = require('../lib/addSubscriber')({
+				errors: errors,
+				knex: global.knex,
+				bookshelf: bookshelf
+			});
+			let Subscriber = require('../lib/models/Subscriber')(bookshelf);
+
+			addSubscriber('email', 'test@test.com', 73)
+			.then(s => {
+				Subscriber.forge({id: s.id}).fetch().then(s => {
+					expect(s.get('userId')).to.equal(73);
+					done();
+				})
+				.catch(done);
+			});
+		});
+
+		it('should store subscriptions if any are specified', function(done) {
+			let bookshelf = require('bookshelf')(global.knex);
+			bookshelf.plugin('registry');
+			let addSubscriber = require('../lib/addSubscriber')({
+				errors: errors,
+				knex: global.knex,
+				bookshelf: bookshelf
+			});
+
+			addSubscriber('email', 'test@test.com', null, ['subscription/1', 'subscription/2', 'subscription/3'])
+			.then(s => {
+				s.subscriptions().fetch().then(subscriptions => {
+					let descriptors = subscriptions.map(s => s.get('descriptor'));
+					expect(descriptors.length).to.equal(3);
+					expect(descriptors.indexOf('subscription/1')).to.be.gte(0);
+					expect(descriptors.indexOf('subscription/2')).to.be.gte(0);
+					expect(descriptors.indexOf('subscription/3')).to.be.gte(0);
+					done();
+				});
+			})
+			.catch(done);
+		});
+	});
 });
